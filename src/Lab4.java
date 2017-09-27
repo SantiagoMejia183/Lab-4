24e9846e575e89db99505b47ba9df3a50465fcc1
SantiagoMejia183 <Santi183@ufl.edu>
2017-09-27 18:45:37 -0400

    Test code

---
+++ src/Lab4.java
@@ -0,0 +1,13 @@
+public class Lab4 {
+
+    public static void main(String args[]){
+
+        int a = 2;
+        int b = -1;
+
+        int sum = a + b;
+
+        System.out.println("Sum is " + sum);
+
+    }
+}
